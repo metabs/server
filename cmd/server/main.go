3d6e1e2548844c471eef920db9bfc47784218014
Damiano Petrungaro <damiano.petrungaro@messagebird.com>
2020-02-24 03:01:05 +0100

    feat(internal): rename pkg server to http

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"net/http"
+	nethttp "net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,8 +11,8 @@ import (
 	"github.com/unprogettosenzanomecheforseinizieremo/server/internal/log"
 
 	database "github.com/unprogettosenzanomecheforseinizieremo/server/internal/db"
+	"github.com/unprogettosenzanomecheforseinizieremo/server/internal/http"
 	"github.com/unprogettosenzanomecheforseinizieremo/server/internal/probe"
-	"github.com/unprogettosenzanomecheforseinizieremo/server/internal/server"
 )
 
 func main() {
@@ -39,22 +39,22 @@ func main() {
 		logger.With("error", err).Fatal("Could not connect to the database.")
 	}
 
-	r := server.NewRouter(logger)
+	r := http.NewRouter(logger)
 	r.Route("/", probe.NewRouter(db))
-	srv := server.New(r)
+	srv := http.New(r)
 
 	done := make(chan struct{}, 1)
 	go func(done chan<- struct{}) {
 		<-ctx.Done()
 
-		logger.Info("Stopping the server...")
+		logger.Info("Stopping the http...")
 
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
 		srv.SetKeepAlivesEnabled(false)
 		if err := srv.Shutdown(ctx); err != nil {
-			logger.With("error", err).Fatal("Could not gracefully shutdown the server.")
+			logger.With("error", err).Fatal("Could not gracefully shutdown the http.")
 		}
 
 		// If you have any metrics or logs that need to be read before the shut down, remove the comment to the next 3 lines
@@ -67,7 +67,7 @@ func main() {
 	}(done)
 
 	logger.Info("Server running...")
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	if err := srv.ListenAndServe(); err != nil && err != nethttp.ErrServerClosed {
 		logger.With("error", err).Fatal("Could not listen and serve.")
 	}
 	<-kill
